e64165b008aebd90456b9c5b514096c5252f2028
quenginedev <quenginedev@gmail.com>
2023-10-16 20:53:10 +0000

    Serialize and Desiralize EntryPoint, Frame and Instruction

--- src/core/instruction/mod.rs
+++ src/core/instruction/mod.rs
@@ -1,21 +1,20 @@
+use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct EntryPoint {
     pub url: String,
     pub page_frames: Vec<Frame>,
 }
 pub const ENTRY_POINT: &str = "^about:srcdoc$";
-
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Frame {
     pub url_matcher: String,
     pub instructions: Vec<Instruction>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Instruction {
-    Click {
-        selector: String,
-    },
-    Wait {
-        timeout: Duration,
-    },
+    Click { selector: String },
+    Wait { timeout: Duration },
 }
